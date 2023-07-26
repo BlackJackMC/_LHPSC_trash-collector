0b6ed25cce1a880a4a33815d9a02f4d54df84439
BlackJackMC <duytu85343@gmail.com>
2023-07-26 15:40:16 +0700

    Add update map function

---
+++ data.py
@@ -0,0 +1,51 @@
+import requests
+import os
+
+def _fetch_data(owner, repo_name, path, access_token=None):
+    base_url = "https://api.github.com/repos"
+    url = f"{base_url}/{owner}/{repo_name}/contents/{path}"
+    
+    headers = {}
+    if access_token:
+        headers["Authorization"] = f"Bearer {access_token}"
+    
+    response = requests.get(url, headers=headers)
+    
+    if response.status_code == 200:
+        contents = response.json()
+        return contents
+    else:
+        raise Exception(f"Failed to fetch contents. Status code: {response.status_code}, Error: {response.text}")
+
+def _save_data(contents, local_folder, access_token=None):
+    headers = {}
+    if access_token:
+        headers["Authorization"] = f"Bearer {access_token}"
+    
+    os.makedirs(local_folder, exist_ok=True)
+    for item in contents:
+        file_name = item["name"]
+        file_download_url = item["download_url"]
+        local_file_path = os.path.join(local_folder, file_name)
+        file_response = requests.get(file_download_url, headers=headers)
+        if file_response.status_code == 200:
+            with open(local_file_path, "wb") as file:
+                file.write(file_response.content)
+        else:
+            raise Exception(f"Failed to download {file_name}. Status code: {file_response.status_code}")
+
+
+def download_map(owner, repo_name, path, local_folder):
+    try:
+        _save_data(_fetch_data(owner, repo_name, path), local_folder)
+    except:
+        raise Exception(f"Failed to update map")
+    else:
+        return True
+
+def main():
+    print("A module to download folder from github (Use in this repo only)")
+    print("Please import from another file to use")
+
+if __name__ == "__main__":
+    main()
---
+++ main.py
@@ -0,0 +1,7 @@
+import data
+
+data.download_map("123khg", 
+                  "Traveling-Trash-Collector-SM-LHPSC-2023",
+                  "generated/data",
+                  "./data")
+
